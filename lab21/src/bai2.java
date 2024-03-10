6ba75c14ed896ff6b4c4f9639698417f91e53aa1
MaiVanPhong <156519917+MaiVanPhong@users.noreply.github.com>
2024-03-10 20:18:31 +0700

    nopbtvn10/3

---
+++ lab21/src/bai2.java
@@ -0,0 +1,13 @@
+package src;
+import java.util.Scanner;
+public class bai2 {
+    public static void main(String[] args){
+        double a;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("nhap so a ");
+        a=sc.nextDouble();
+        if(a%2==0){
+            System.out.println("so chan");
+        }else System.out.println("so le");
+    }
+}
---
+++ lab21/src/bai3.java
@@ -0,0 +1,23 @@
+package src;
+import java.util.Scanner;
+
+public class bai3 {
+    public static void main(String[] args) {
+        int namsinh;
+        String ten;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ten cua ban :");
+        ten = sc.nextLine();
+        System.out.println("nhap nam sinh cua ban :");
+        namsinh = sc.nextInt();
+        if (2024-namsinh<16){
+        System.out.println("Ban "+ ten +" o do tuoi vi thanh nien");
+        }
+        else if (2024-namsinh>=16 && 2024-namsinh<18){
+        System.out.println("Ban "+ ten +" o do tuoi truong thanh");
+        }
+        else{
+        System.out.println("Ban "+ ten +" da gia");
+        }
+    }
+}
