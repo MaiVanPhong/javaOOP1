3b34871abdc9e3baa1d0b8e266aaf62c6d8e8ff6
MaiVanPhong <156519917+MaiVanPhong@users.noreply.github.com>
2024-05-23 14:03:40 +0700

    nopbtngay17/5

--- lab2/src/baitapc4/bai1.java
+++ lab2/src/baitapc4/bai1.java
@@ -6,20 +6,20 @@ public class bai1{
     public static void main(String[] args){
         ArrayList<Integer> arrlistint = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập vào số lượng phần tử: ");
+        System.out.print("nhap vao so phan tu: ");
         int n = scanner.nextInt();
-        System.out.println("Nhập vào các phần tử:");
+        System.out.println("nhap vao cac phan tu:");
         for (int i = 0; i < n; i++) {
             int number = scanner.nextInt();
             arrlistint.add(number);
         }
         int max = Collections.max(arrlistint);
-        System.out.println("Số nguyên lớn nhất trong dãy là: " + max);
-        System.out.print("Nhập vào số nguyên để xoá: ");
+        System.out.println("so nguyen lon nhat trong day la: " + max);
+        System.out.print("nhap vao so nguyen de xoa: ");
         int deleteNumber = scanner.nextInt();
         arrlistint.removeIf(num -> num == deleteNumber);
         Collections.sort(arrlistint);
-        System.out.println("Dãy số sau khi xoá và sắp xếp:");
+        System.out.println("day so sau khi xoa va sap xep:");
         for (int number : arrlistint) {
             System.out.print(number + " ");
         }
